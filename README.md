a9639e0bc1920682f2f162c9ec2bfdf173229e3d
Michael Mack <mack@psych.utoronto.ca>
2018-09-14 21:35:30 -0400

    project init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scene-cat
 data and skeleton script for PS1
+
---
+++ group_analysis.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+scene-cat problem set for PSY 1210 - Fall 2018
+
+@author: Michael Mack
+"""
+
+
